c2b1f1ad6dbb93ea9fc37af73b61075e9aa17916
MadeR228 <michaelhabor37@gmail.com>
2025-04-26 17:32:57 +0300

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 
@@ -26,5 +26,5 @@ app.use((req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Сервер - http://localhost:${port}`);
-}); 
+    console.log(`Сервер запущен на порту ${port}`);
+});
