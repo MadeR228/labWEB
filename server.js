ce40570d100172313588641c1002f96b1535aa35
MadeR228 <michaelhabor37@gmail.com>
2025-04-26 19:02:45 +0300

    commit

--- server.js
+++ server.js
@@ -6,23 +6,23 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/public/"));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public",'main.html'));
+    res.sendFile(path.join(__dirname, 'public','main.html'));
 });
 
 app.get('/guess-number', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", 'numbers.html'));
+    res.sendFile(path.join(__dirname, 'public', 'numbers.html'));
 });
 
 app.get('/sequence', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", 'sequence.html'));
+    res.sendFile(path.join(__dirname, 'public', 'sequence.html'));
 });
 
 app.get('/image', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", 'images.html'));
+    res.sendFile(path.join(__dirname, 'public', 'images.html'));
 });
 
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 app.listen(port, () => {
