4e812438c923ef8786ddc2748c431e80f07be757
MadeR228 <michaelhabor37@gmail.com>
2025-04-26 17:59:34 +0300

    commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 
@@ -22,4 +23,8 @@ app.get('/image', (req, res) => {
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Сервер - http://localhost:${port}`);
 });
