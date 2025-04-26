81afdb45263c2ee93ffa44930a6b1a646dcc3387
MadeR228 <michaelhabor37@gmail.com>
2025-04-26 18:27:58 +0300

    commit

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 
-app.get('./', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'main.html'));
 });
 
-app.get('./guess-number', (req, res) => {
+app.get('/guess-number', (req, res) => {
     res.sendFile(path.join(__dirname, 'numbers.html'));
 });
 
-app.get('./sequence', (req, res) => {
+app.get('/sequence', (req, res) => {
     res.sendFile(path.join(__dirname, 'sequence.html'));
 });
 
-app.get('./image', (req, res) => {
+app.get('/image', (req, res) => {
     res.sendFile(path.join(__dirname, 'images.html'));
 });
 
